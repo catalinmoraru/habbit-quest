2e5f2598bd24bbbf990e62b9c5064d65970a59f8
catalin.moraru <catalin.moraru@bearingpoint.com>
2017-08-22 13:36:04 +0300

    minor change

--- src/main/java/com/redhat/developers/msa/ola/OlaController.java
+++ src/main/java/com/redhat/developers/msa/ola/OlaController.java
@@ -80,6 +80,6 @@ public class OlaController {
     @RequestMapping(method = RequestMethod.GET, value = "/health")
     @ApiOperation("Used to verify the health of the service")
     public String health() {
-        return "I'm ok";
+        return "I'm ok Alex, I'm learning something new";
     }
 }
