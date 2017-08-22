b768b2ff288a6b596592c968efca161e6c10268f
catalin.moraru <catalin.moraru@bearingpoint.com>
2017-08-22 17:10:59 +0300

    added Topic and Courses Endpoints and pom dependecies

---
+++ src/main/java/com/redhat/developers/msa/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.redhat.developers.msa.topic;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+}
