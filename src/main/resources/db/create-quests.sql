a7f2d7e3a9d0ed187b11ba28e6087b01d9bb8500
catalin.moraru <catalin.moraru@bearingpoint.com>
2017-08-23 17:17:57 +0300

    added FK

--- src/main/resources/db/create-quests.sql
+++ src/main/resources/db/create-quests.sql
@@ -1,7 +1,13 @@
 --DROP TABLE users IF EXISTS;
 
 CREATE TABLE quests (
-  id         INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name VARCHAR(30),
   description  VARCHAR(50)
+);
+
+create TABLE questsboss(
+    id INTEGER PRIMARY KEY,
+    questid INTEGER references quests(id),
+    name VARCHAR(30)
 );
--- src/main/resources/db/insert-quests.sql
+++ src/main/resources/db/insert-quests.sql
@@ -1,3 +1,8 @@
 INSERT INTO quests VALUES (1, 'fire', 'fox');
 INSERT INTO quests VALUES (2, 'water', 'element');
 INSERT INTO quests VALUES (3, 'poison', 'dagger');
+
+
+INSERT INTO questsboss VALUES (1, 1, 'fox');
+INSERT INTO questsboss VALUES (2, 2, 'element');
+INSERT INTO questsboss VALUES (3, 3, 'dagger');
