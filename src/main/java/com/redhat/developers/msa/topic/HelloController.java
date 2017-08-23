261762169c81b425227d37fe3bf3945476871ed1
catalin.moraru <catalin.moraru@bearingpoint.com>
2017-08-23 17:23:02 +0300

    added code to populate :)

--- src/main/java/com/redhat/developers/msa/topic/HelloController.java
+++ src/main/java/com/redhat/developers/msa/topic/HelloController.java
@@ -1,5 +1,6 @@
 package com.redhat.developers.msa.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/hello")
     public String sayHi(){
+
+        Topic topic1 = new Topic("1","topic1","description1");
+        Topic topic2 = new Topic("2","topic2","description2");
+        Topic topic3 = new Topic("3","topic3","description3");
+        topicService.addTopic(topic1);
+        topicService.addTopic(topic2);
+        topicService.addTopic(topic2);
+
         return "Hi";
     }
 }
